275fea705cab7d24886b59311518e6332f98506f
Xiaoxiong Xing <xingxiaoxiong@frontier.hokudai.ac.jp>
2016-01-23 14:34:13 +0800

    Initial Commit

---
+++ OpenGLDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLDemo
+//
+//  Created by xingxiaoxiong on 1/23/16.
+//  Copyright © 2016 BettyBearStudio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
